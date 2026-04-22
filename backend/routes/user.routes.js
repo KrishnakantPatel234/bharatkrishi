cef089a687d2748854ffb8d59707e7e2d8f05d75
Krishna <kp8353156@gmail.com>
2026-04-22 09:01:15 +0530

    removing errors

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import { getUser , getUsers} from "../controllers/user.controllers.js";
+import {getMyPosts} from "../controllers/post.controllers.js";
 
 const router = Router();
 
 router.get("/" , getUsers);
 router.get("/:id" , getUser);
-router.get("/:id/posts" , );
+router.get("/:id/posts" , getMyPosts);
 
 export default router;
