3016595ee07296049779e72b0eecc637fa99fb7d
Krishna <kp8353156@gmail.com>
2026-05-11 18:08:26 +0530

    added mitra AI feature

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL : "http://localhost:3000/api/",
-    // baseURL: "https://bharatkrishi.vercel.app/",
+    // baseURL : "http://localhost:3000/api/",
+    baseURL: "https://bharatkrishi.vercel.app/",
     withCredentials: true,  
 });
 
