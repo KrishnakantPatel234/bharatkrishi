52eeca8a51a2600a2f470c6b36228dd4d5a70fa2
Krishna <kp8353156@gmail.com>
2026-05-12 08:28:39 +0530

    removed port error

--- backend/config/chatbot.js
+++ backend/config/chatbot.js
@@ -3,9 +3,13 @@ dotenv.config();
 
 import OpenAI from "openai";
 
+if (!process.env.GROQ_API_KEY) {
+  throw new Error("GROQ_API_KEY is missing");
+}
+
 const client = new OpenAI({
   apiKey: process.env.GROQ_API_KEY,
   baseURL: "https://api.groq.com/openai/v1",
 });
 
-export default client;   
+export default client;
--- backend/server.js
+++ backend/server.js
@@ -50,11 +50,6 @@
     app.use("/api/comments" , commentRoutes);
     app.use("/api/mitra" , chatRoutes);
 
-
-    export default app;
-
-    if (process.env.NODE_ENV !== 'production') {
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    }
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
